595a8992faaf9bbdfada47b54a590870e32ee745
Jolita <peciokiene.jolita@gmail.com>
2023-12-07 16:19:41 +0200

    Implement Before/After annotations

--- src/test/java/lt/jolita/demo/SeleniumDemo.java
+++ src/test/java/lt/jolita/demo/SeleniumDemo.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -14,28 +15,11 @@ public class SeleniumDemo {
 
     WebDriver driver;
 
-    @Test // atidarome puslapi per chrome
-    public void openDemoQaWithChrome() {
-        WebDriverManager.chromedriver().setup();
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/text-box");
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.quit();
-    }
-
-    @Test
-    public void testFullNameProvidingNameHarryPotterAndSubmitButton() {
-        String fullName = "Harry Potter";
-        String expectedResult = "Harry Potter";
-        String actualResult;
-
-        WebDriverManager.chromedriver().setup();
+                setup();
 
 //         solution for MacOs
 //        driver.get("chrome://settings/");
@@ -50,8 +34,32 @@ public class SeleniumDemo {
         option.addArguments("--force-device-scale-factor=0.70");
         option.addArguments();
 
-        WebDriver driver = new ChromeDriver(option);
+        driver = new ChromeDriver(option);
         driver.get("https://demoqa.com/text-box"); // cia atidarom narsykle
+    }
+
+    @Test // atidarome puslapi per chrome
+    public void openDemoQaWithChrome() {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/text-box");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
+
+    @Test
+    public void testFullNameProvidingNameHarryPotterAndSubmitButton() {
+        String fullName = "Harry Potter";
+        String expectedResult = "Harry Potter";
+        String actualResult;
+
+         // cia atidarom narsykle
 
 //        WebElement inputUserName = driver.findElement(By.id("userName"));
         WebElement inputUserName = driver.findElement(By.xpath("//input[@id='userName']"));
@@ -67,7 +75,6 @@ public class SeleniumDemo {
                 actualResult.contains(expectedResult),
                 "\nActual: %s,\nExpected: %s".formatted(actualResult, expectedResult)
         );
-        driver.quit();
     }
 
     @Test
@@ -75,16 +82,6 @@ public class SeleniumDemo {
         String fullEmail = "harry.potter@mail.com";
         String expectedResult = "harry.potter@mail.com";
         String actualResult;
-
-        WebDriverManager.chromedriver().setup();
-
-        ChromeOptions option = new ChromeOptions();
-        option.addArguments("start-maximized"); // cia isdidina full screen
-        option.addArguments("--force-device-scale-factor=0.70");
-        option.addArguments();
-
-        WebDriver driver = new ChromeDriver(option);
-        driver.get("https://demoqa.com/text-box"); // cia atidarom narsykle
 
         WebElement inputEmail = driver.findElement(By.xpath("//input[@id='userEmail']"));
         //xpath paemem is narsykles per RMB -> Inspect, crtl+F ir surasem ranka //input[@id='userName']
@@ -98,10 +95,10 @@ public class SeleniumDemo {
                 actualResult.contains(expectedResult),
                 "\nExpected: %s, \nActual: %s".formatted(actualResult, expectedResult)
         );
-        driver.quit();
     }
 
-
-
-
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 }
